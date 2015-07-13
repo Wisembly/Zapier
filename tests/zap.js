ceeb44255bf724cb2f3154ce56a4786422a76b9c
Guillaume Potier <cobalt2760@gmail.com>
2015-07-13 15:22:37 +0200

    - added first impl of meeting hooks

--- tests/zap.js
+++ tests/zap.js
@@ -15,12 +15,24 @@ var verifySchema = function (schema, object) {
 };
 
 describe('Test Zap code', function () {
-    it('should be an object', function () {
-        expects(Zap).to.be.an('object');
+    describe('Misc', function () {
+        it('should be an object', function () {
+            expects(Zap).to.be.an('object');
+        });
+
+        it('should have a getUser method', function () {
+            expects(Zap._getUser({
+                foo: "bar",
+                id: "Us3rTYh",
+                name: "Name",
+                email: "email@host.ext",
+                other: "other"
+            })).to.eql({ name: "Name", email: "email@host.ext" });
+        });
     });
 
     describe('Action Items', function () {
-        var data = {"task":{"id":"O277c59","title":"Et cette t\u00e2chinette ?","created_at":"2015-07-10T16:54:59+00:00","class_name":"Task","due_date":null,"note":"tcbd4af","due_meeting":"y4821f2","assignees":["q7c05f1","ef90235"]},"notes":[{"id":"tcbd4af","title":"Grosse grosse note","type":"default","created_at":"2015-07-10T16:50:39+00:00","class_name":"Note","position":3,"item":"Ebfb610"}],"agendas":[{"id":"Ebfb610","title":"Set the goal of the meeting","created_at":"2015-07-10T09:29:12+00:00","class_name":"Item","position":0,"meeting":"Fe29206"}],"meetings":[{"id":"Fe29206","persisted":true,"name":"A demonstration meeting with Solid","description":"The purpose of this meeting is to show you how to run a productive and actionable meeting, with a little help from Solid.","provider":"wisembly","expected_start":"2015-07-10T09:24:12+00:00","expected_end":"2015-07-10T09:54:12+00:00","real_start":"2015-07-10T09:24:12+00:00","real_end":null,"preparation":"good","roti":{"worthless":{"count":0,"percent":0},"negative":{"count":0,"percent":0},"ok":{"count":0,"percent":0},"good":{"count":0,"percent":0},"perfect":{"count":0,"percent":0},"total":0},"blocked":false,"is_fixture":true,"is_recurring":false,"class_name":"Meeting"},{"id":"y4821f2","persisted":true,"name":"Happy Monday","description":"Point hebdo\n\nView meeting on Solid : https:\/\/solid.wisembly.com\/meetings\/we16f70","provider":"google","expected_start":"2015-07-13T17:00:00+00:00","expected_end":"2015-07-13T19:00:00+00:00","real_start":null,"real_end":null,"preparation":"bad","roti":{"worthless":{"count":0,"percent":0},"negative":{"count":0,"percent":0},"ok":{"count":0,"percent":0},"good":{"count":0,"percent":0},"perfect":{"count":0,"percent":0},"total":0},"blocked":false,"is_fixture":false,"is_recurring":true,"class_name":"Meeting"}],"users":[{"id":"q7c05f1","name":"Romain David","email":"romain@wisembly.com","company":null,"root":true,"avatar":null,"timezone_offset":60,"subscriptions":{"digests":{"meetings":true}}},{"id":"ef90235","name":"Andre\u00ef Vestemeanu","email":"andrei@wisembly.com","company":null,"root":true,"avatar":null,"timezone_offset":60,"subscriptions":{"digests":{"meetings":true}}}],"sender":{"id":"A807971","name":"Guillaume Potier","email":"guillaume@wisembly.com","company":null,"root":true,"avatar":null,"timezone_offset":120,"subscriptions":{"digests":{"meetings":true}}}};
+        var data = require('../hooks/task.json');
 
         it('should have a valid schema', function () {
             var result = Zap.action_item_catch_hook({ cleaned_request: data, request: { headers: [] } });
@@ -32,10 +44,8 @@ describe('Test Zap code', function () {
 
             request.headers[Zap.header_event_key] = "foo";
             expects(Zap.action_item_catch_hook({ cleaned_request: {}, request: request }).length).to.be(0);
-
             request.headers[Zap.header_event_key] = "task.create";
             expects(Zap.action_item_catch_hook({ cleaned_request: data, request: request })).to.be.an('object');
-
             request.headers[Zap.header_event_key] = "task.edit";
             expects(Zap.action_item_catch_hook({ cleaned_request: data, request: request })).to.be.an('object');
         });
@@ -43,14 +53,15 @@ describe('Test Zap code', function () {
         it('should transform hook sent data into something usable in Zapier', function () {
             var result = Zap.action_item_catch_hook({ cleaned_request: data, request: { headers: [] } });
             expects(result).to.be.an('object');
-            expects(result.action_item_name).to.be('Et cette tâchinette ?');
-            expects(result.created_at).to.be('2015-07-10T16:54:59+00:00');
+            expects(result.action_item_name).to.be('A delightful task to do');
+            expects(result.created_at).to.be('2015-07-10T16:53:03+00:00');
             expects(result.due_for).to.be('2015-07-13T17:00:00+00:00');
             expects(result.action_item_context).to.be('Grosse grosse note');
             expects(result.sender.name).to.be('Guillaume Potier');
-            expects(result.assignees_names).to.be('Romain David, Andreï Vestemeanu');
+            expects(result.assignees_names).to.be('Guillaume Potier, Romain David, Andreï Vestemeanu');
             expects(result.meeting_url).to.be('https://solid.wisembly.com/meetings/Fe29206');
             expects(result.due_meeting_url).to.be('https://solid.wisembly.com/meetings/y4821f2');
+            expects(result.assignees[0]).to.eql({ name: "Guillaume Potier", email: "guillaume@wisembly.com" });
         });
 
         it('should handle due_for', function () {
@@ -60,6 +71,42 @@ describe('Test Zap code', function () {
             data.task.due_meeting = null;
             result = Zap.action_item_catch_hook({ cleaned_request: data, request: { headers: [] } });
             expects(result.due_for).to.be(data.task.due_date);
+        });
+
+        it('should transform users', function () {
+            var result = Zap.action_item_catch_hook({ cleaned_request: data, request: { headers: [] } });
+            expects(result.sender).to.eql({ name: "Guillaume Potier", email: "guillaume@wisembly.com" })
+        });
+    });
+
+    describe('Meetings', function () {
+        var data = require('../hooks/meeting.json');
+
+        it('should have a valid schema', function () {
+            var result = Zap.meeting_catch_hook({ cleaned_request: data, request: { headers: [] } });
+            expects(verifySchema(require('../schemas/meeting.json'), result)).to.be(true);
+        });
+
+        it('should only work for meeting hooks', function () {
+            var request = { headers: [] };
+
+            request.headers[Zap.header_event_key] = "task.create";
+            expects(Zap.meeting_catch_hook({ cleaned_request: {}, request: request }).length).to.be(0);
+            request.headers[Zap.header_event_key] = "meeting.start";
+            expects(Zap.meeting_catch_hook({ cleaned_request: data, request: request })).to.be.an('object');
+            request.headers[Zap.header_event_key] = "meeting.stop";
+            expects(Zap.meeting_catch_hook({ cleaned_request: data, request: request })).to.be.an('object');
+        });
+
+        it('should handle agenda object', function () {
+            var result = Zap.meeting_catch_hook({ cleaned_request: data, request: { headers: [] } });
+
+            expects(result.agenda.length).to.be(4);
+            expects(result.agenda[0].title).to.be('Set the goal of the meeting');
+            expects(result.agenda[0].created_at).to.be('2015-07-10T09:29:12+00:00');
+            expects(result.agenda[0].notes.length).to.be(6);
+            expects(result.agenda[0].notes[0].title).to.be('The goal of this demonstration meeting has been set by Solid earlier. It describes what needs to happen as a result of the meeting.');
+            expects(result.agenda[0].notes[0].created_at).to.be('2015-07-10T09:29:12+00:00');
         });
     });
 });
--- zap.js
+++ zap.js
@@ -2,25 +2,22 @@ var Zap = {
     header_event_key: 'Http-X-Wisembly-Event',
 
     action_item_catch_hook: function (bundle) {
-        var data = bundle.cleaned_request,
-            headers = bundle.request.headers,
-            assignees = [],
+
+        // works only for task events
+        if (bundle.request.headers[this.header_event_key]
+            && !bundle.request.headers[this.header_event_key].match(/task./)) {
+            return [];
+        }
+
+        var that = this,
+            data = bundle.cleaned_request,
             task = data.task,
             users = _.object(_.pluck(data.users, 'id'), data.users),
             notes = _.object(_.pluck(data.notes, 'id'), data.notes),
             agendas = _.object(_.pluck(data.agendas, 'id'), data.agendas),
             meetings = _.object(_.pluck(data.meetings, 'id'), data.meetings),
-            meeting_url = 'https://solid.wisembly.com/meetings/{hash}';
-
-        // work only for task events
-        if (headers[this.header_event_key] && !headers[this.header_event_key].match(/task./)) {
-            return [];
-        }
-
-        // handle assigneess array
-        for (var i = 0; i < task.assignees.length; i++) {
-            assignees.push(users[task.assignees[i]]);
-        }
+            meeting_url = 'https://solid.wisembly.com/meetings/{hash}',
+            assignees = _.map(task.assignees, function (id) { return that._getUser(users[id]); });
 
         // get current meeting through note->item->meeting --'
         var current_meeting = meetings[agendas[notes[task.note].item].meeting];
@@ -31,7 +28,7 @@ var Zap = {
             created_at: task.created_at,
             due_for: null !== task.due_meeting ? meetings[task.due_meeting].expected_start : task.due_date,
             action_item_context: notes[task.note].title,
-            sender: _.pick(data.sender, 'id', 'name', 'email'),
+            sender: this._getUser(data.sender),
             assignees: assignees,
             assignees_names: _.pluck(assignees, 'name').join(', '),
             meeting_url: meeting_url.replace('{hash}', current_meeting.id),
@@ -41,6 +38,50 @@ var Zap = {
 
     meeting_catch_hook: function (bundle) {
 
+        // works only for meeting events
+        if (bundle.request.headers[this.header_event_key]
+            && !bundle.request.headers[this.header_event_key].match(/meeting./)) {
+            return [];
+        }
+
+        var that = this,
+            data = bundle.cleaned_request,
+            meeting = data.meeting,
+            participants = [],
+            notes = _.object(_.pluck(data.notes, 'id'), data.notes),
+            users = _.object(_.pluck(data.users, 'id'), data.users),
+            participants = _.map(meeting.participants, function (id) { return that._getUser(users[id]); }),
+            meeting_url = 'https://solid.wisembly.com/meetings/{hash}';
+
+        var agenda = _.map(data.agendas, function (agenda) {
+            agenda = _.pick(agenda, 'title', 'created_at', 'notes');
+            agenda.notes = _.map(agenda.notes, function (id) {
+                return _.pick(notes[id], 'title', 'created_at');
+            });
+
+            return agenda;
+        });
+
+        // see schemas/meeting.json
+        return {
+            name: meeting.name,
+            description: meeting.description,
+            expected_start: meeting.expected_start,
+            expected_end: meeting.expected_end,
+            real_start: meeting.real_start,
+            real_end: meeting.real_end,
+            preparation: meeting.preparation,
+            is_recurring: meeting.is_recurring,
+            owner: this._getUser(users[meeting.owner]),
+            sender: this._getUser(data.sender),
+            participants: participants,
+            agenda: agenda,
+            meeting_url: meeting_url.replace('{hash}', meeting.id)
+        };
+    },
+
+    _getUser: function (user) {
+        return _.pick(user, 'name', 'email');
     }
 };
 
